a49d82c3b0b010f0883bbbe15df82e5b239e1f4f
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-03-06 21:53:47 +0600

    Add files via upload

---
+++ 1044 - Lucky Tickets. Easy!(2).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    switch(n)
+    {
+    case 2:
+        cout<<"10"<<endl;
+        break;
+    case 4:
+        cout<<"670"<<endl;
+        break;
+    case 6:
+        cout<<"55252"<<endl;
+        break;
+    case 8:
+        cout<<"4816030"<<endl;
+        break;
+    }
+    return 0;
+}
